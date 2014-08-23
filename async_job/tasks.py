fcbf82f17c2e288968915df5d1160e020050d379
Francis Wang <francis@bposolutions.com>
2014-08-22 18:27:22 -0700

    More test on retry and timeout

--- async_job/tasks.py
+++ async_job/tasks.py
@@ -10,20 +10,37 @@ from celery.exceptions import SoftTimeLimitExceeded, MaxRetriesExceededError
 def run_job(job_id):
     Job.objects.get(pk=job_id).run_job()
 
-@celery_app.task(bind=True, time_limit=2, soft_time_limit=1, default_retry_delay=1, max_retries=2)
+@celery_app.task(bind=True, soft_time_limit=1, default_retry_delay=1, max_retries=2)
 def long_running_job(self):
     try:
         time.sleep(10)
         return True
     except SoftTimeLimitExceeded as e:
         print 'Retrying'
-        # self.update_state(state=states.RETRY)
-        return self.retry(exc=Exception('Timeout'), propagates=True)
+        return self.retry(propagates=True) # propagating is a must
 
 def do_long_running_job():
     r = long_running_job.delay()
     while r.state not in ('FAILURE', 'SUCCESS'):
         print "waiting..", r.state
         time.sleep(0.5)
+    print r.state
     return r
 
+@celery_app.task(bind=True, soft_time_limit=1, default_retry_delay=1, max_retries=2)
+def enough_time_after_retry(self):
+    try:
+        time.sleep(2.5-self.request.retries)
+        return "retried result"
+    except SoftTimeLimitExceeded as e:
+        print 'Retrying'
+        return self.retry(propagates=True)
+
+def do_enough_time_after_retry():
+    r = enough_time_after_retry.delay()
+    # r.wait()
+    while r.state not in ('FAILURE', 'SUCCESS'):
+        print "waiting..", r.state
+        time.sleep(0.5)
+    print r.state
+    return r.get()
