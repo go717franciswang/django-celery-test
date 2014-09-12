53ce6a9358edf5656064bbb12752b68f12d02976
Francis Wang <go717franciswang@hotmail.com>
2014-09-11 21:51:05 -0700

    Test workflow

--- async_job/tasks.py
+++ async_job/tasks.py
@@ -5,6 +5,7 @@ from async_job.models import Job
 from .celery import app as celery_app
 import time
 from celery.exceptions import SoftTimeLimitExceeded, MaxRetriesExceededError
+from celery import group, chain
 
 @shared_task
 def run_job(job_id):
@@ -44,3 +45,21 @@ def do_enough_time_after_retry():
         time.sleep(0.5)
     print r.state
     return r.get()
+
+@celery_app.task()
+def preparation_task():
+    print "start preparation"
+    time.sleep(1)
+    print "end preparation"
+
+@celery_app.task()
+def final_task():
+    print "start final task"
+    time.sleep(1)
+    print "end final task"
+
+def preparations_and_final():
+    g = group(preparation_task.s() for i in xrange(100))
+    c = chain(g, final_task.si())
+    c.apply_async()
+
